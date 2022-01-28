2d2712347105b79dfe5257ca88d18f4e9425d868
Mmackz <matthewmackenz@gmail.com>
2022-01-27 19:19:03 -0800

    ✨ add ip address

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami", (req, res) => {
-   const ipaddress = req.connection.remoteAddress;
+   const ipaddress = req.ip;
    const language = req.headers["accept-language"];
    const software = req.headers["user-agent"];
    res.json({ ipaddress, language, software });
